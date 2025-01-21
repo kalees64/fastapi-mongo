6fe2d3dadb689da61f9d8f28c46b0358b4f5fc4b
Kaleeswaran V <vkalees64@gmail.com>
2025-01-21 07:45:17 +0530

    chore: initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi_cli.cli import dev
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+from pathlib import Path
+
+app = FastAPI(title="FastAPI + MongoDB",version="0.0.1")
+
+class Response(BaseModel):
+    data:str
+
+@app.get("/",tags=["Default"])
+def root():
+    return Response(data="API is running")
+
+if __name__ == "__main__":
+    # uvicorn.run("main:app", host="127.0.0.1", port=3000, reload=True)
+    dev(port=3000,host="127.0.0.1",reload=True,path=Path("main.py"))
